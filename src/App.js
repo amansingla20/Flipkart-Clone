591a946751db72e8257e310bc843ac0476d229cc
AMAN SINGLA <amansingla222@gmail.com>
2021-05-06 14:45:17 +0530

    Implemented SecondNav and Image added after Second Nav

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Data } from "./Data";
 import { useState, useEffect } from "react";
 import Card from "./components/Card";
 import Carousel from "./components/Carousel";
+import SecondNav from "./components/SecondNav";
 
 function App() {
   const [category, setCategory] = useState([]);
@@ -23,6 +24,13 @@ function App() {
   return (
     <div className="app">
       <Header />
+      <SecondNav />
+      <div className="image-after-second-nav">
+        <img
+          src="https://rukminim1.flixcart.com/www/3376/3376/promos/30/04/2021/db07844f-6b26-48aa-ab7c-67011d04fe6e.png?q=90"
+          alt=""
+        />
+      </div>
       <Carousel />
       <div className="Products">
         {category.map((item) => (
---
+++ src/components/SecondNav.jsx
@@ -0,0 +1,368 @@
+import './SecondNav.css';
+
+import React from 'react'
+
+const SecondNav = () => {
+    return (
+        <div className='secondNav'>
+             <ul className="width1250 flex space-btw  opacity">
+                <li className="dropdown"><div className="dropbtn" role="button">Electronics <svg width="4.7" height="8" viewBox="0 0 16 27" xmlns="http://www.w3.org/2000/svg" class="_34Yjv1"><path d="M16 23.207L6.11 13.161 16 3.093 12.955 0 0 13.161l12.955 13.161z" fill="#000000" class="_14jSvk"></path></svg></div>
+                    <div className="width1250 dropdown-content">
+                        <div className="flex space-btw ">
+                            <ul>
+                                <h4>Mobiles</h4>
+                                <li>Mi</li>
+                                <li>Samsung</li>
+                                <li>Apple</li>
+                                <li>Vivo</li>
+                            </ul>
+                            <ul>
+                                <h4>Laptops</h4>
+                                <li>Gaming Laptops</li>
+                                <li>Normal Laptops</li>
+                            </ul>
+                            <ul>
+                                <h4>Computer Accessories</h4>
+                                <li>Pen Drives</li>
+                                <li>External Hard Drive</li>
+                                <li>Mouse</li>
+                            </ul>
+                            <ul>
+                                <h4>Mobile Accessories</h4>
+                                <li>Headphones & Headsets</li>
+                                <li>Power Banks</li>
+                                <li>Screen Guards</li>
+                                <li>Memory Cards</li>
+                            </ul>
+                            <ul>
+                                <h4>Speakers</h4>
+                                <li>Mobile Speakers</li>
+                                <li>Home Speakers</li>
+                                <li>Home Theatres</li>
+                                <li>Loud Speakers</li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li className="dropdown"><div className="dropbtn" role="button">TVs & Appliances <svg width="4.7" height="8" viewBox="0 0 16 27" xmlns="http://www.w3.org/2000/svg" class="_34Yjv1"><path d="M16 23.207L6.11 13.161 16 3.093 12.955 0 0 13.161l12.955 13.161z" fill="#000000" class="_14jSvk"></path></svg></div>
+                    <div className="width1250 dropdown-content">
+                        <div className="flex space-btw ">
+                            <ul>
+                                <h4>Top Brands</h4>
+                                <li>Mi</li>
+                                <li>Vu</li>
+                                <li>Samsung</li>
+                                <li>LG</li>
+                            </ul>
+                            <ul>
+                                <h4>Air Conditioners</h4>
+                                <li>Inverter AC</li>
+                                <li>Split AC</li>
+                                <li>Window AC</li>
+                            </ul>
+                            <ul>
+                                <h4>Header Three</h4>
+                                <li>One</li>
+                                <li>Two</li>
+                                <li>Three</li>
+                                <li>FOur</li>
+                            </ul>
+                            <ul>
+                                <h4>Shop By Brand</h4>
+                                <li>LG</li>
+                                <li>Hitachi</li>
+                                <li>Carrier</li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li className="dropdown"><div className="dropbtn" role="button">Men <svg width="4.7" height="8" viewBox="0 0 16 27" xmlns="http://www.w3.org/2000/svg" class="_34Yjv1"><path d="M16 23.207L6.11 13.161 16 3.093 12.955 0 0 13.161l12.955 13.161z" fill="#000000" class="_14jSvk"></path></svg></div>
+                    <div className="width1250 dropdown-content">
+                        <div className="flex space-btw ">
+                            <div>
+                                <ul>
+                                    <h4>Foot Wear</h4>
+                                    <li>Sport Shoes</li>
+                                    <li>Casual Shoes</li>
+                                    <li>Formal Shoes</li>
+                                    <li>Sandal & Floaters</li>
+                                    <li>Loafers</li>
+                                    <li>Flip Flops</li>
+                                    <li>Boots</li>
+                                    <li>Running Shoes</li>
+                                    <li>Sneakers</li>
+                                </ul>
+                                <ul>
+                                    <h4>Men's Grooming</h4>
+                                    <li>Deodorants</li>
+                                    <li>Perfumes</li>
+                                    <li>Beard Care & Grooming</li>
+                                    <li>Shaving & Aftershave</li>
+                                    <li>Sexual Wellness</li>
+                                </ul>
+                            </div>
+                            <div>
+                                <ul>
+                                    <h4>Clothing</h4>
+                                    <h4>Top Wear</h4>
+                                    <li>T-Shirt</li>
+                                    <li>Formal Shirt</li>
+                                    <li>Casual Shirt</li>
+                                </ul>
+                                <ul>
+                                    <h4>Bottom Wear</h4>
+                                    <li>Jeans</li>
+                                    <li>Casual Trouser</li>
+                                    <li>Formal Trousers</li>
+                                    <li>Track Pants</li>
+                                    <li>Shorts</li>
+                                    <li>Cargos</li>
+                                    <li>Three Fourths</li>
+                                    <h4>Suit, Blazer & Waistcoats</h4>
+                                    <h4>Tie, Socks, Cap & More</h4>
+                                    <h4>Fabrics</h4>
+                                </ul>
+                            </div>
+                            <div>
+                                <ul>
+                                    <h4>Winter Wear</h4>
+                                    <li>Sweatshirt</li>
+                                    <li>Jackets</li>
+                                    <li>Sweater</li>
+                                    <li>Track Suite</li>
+                                </ul>
+                                <ul>
+                                    <h4>Ethnic Wear</h4>
+                                    <li>Kurta</li>
+                                    <li>Ethnic Sets</li>
+                                    <li>Sherwanis</li>
+                                    <li>Ethnic Pyjama</li>
+                                    <li>Dhoti</li>
+                                    <li>Lungi</li>
+                                </ul>
+                                <ul>
+                                    <h4>Innerwear & Loungewear</h4>
+                                    <li>Briefs & Trunks</li>
+                                    <li>Vests</li>
+                                    <li>Boxers</li>
+                                    <li>Pyjamas and Lounge Pants</li>
+                                    <li>Thermals</li>
+                                    <li>Night Suits</li>
+                                    <li>Raincoats & Windcheaters</li>
+                                </ul>
+                            </div>
+                            <div>
+                                <ul>
+                                    <h4>Watches</h4>
+                                    <li>Fastrack</li>
+                                    <li>Casio</li>
+                                    <li>Titan</li>
+                                    <li>Fossil</li>
+                                    <li>Sonata</li>
+                                </ul>
+                                <ul>
+                                    <h4>Accessories</h4>
+                                    <li>Backpacks</li>
+                                    <li>Wallets</li>
+                                    <li>Belts</li>
+                                    <li>Sunglasses</li>
+                                    <li>Luggage & Travel</li>
+                                    <li>Frames</li>
+                                    <li>Jewellery</li>
+                                    <h4>Sports & Fitness Store</h4>
+                                </ul>
+                            </div>
+                            <div>
+                                <ul>
+                                    <h4>Smart Watches</h4>
+                                    <h4>Smart Bands</h4>
+                                    <h4>Personal Care Appliances</h4>
+                                    <li>Trimmers</li>
+                                    <li>Shavers</li>
+                                    <li>Grooming Kits</li>
+                                </ul>
+                                <ul>
+                                    <h4>Featured</h4>
+                                    <h4>Swatches Store</h4>
+                                    <h4>Bags & Wallet</h4>
+                                    <li>T-Shirt Store</li>
+                                    <li>Adidas</li>
+                                    <li>Beardo</li>
+                                    <li>Reebok</li>
+                                    <li>Skechers</li>
+                                    <li>Nike</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li className="dropdown"><div className="dropbtn" role="button">Women <svg width="4.7" height="8" viewBox="0 0 16 27" xmlns="http://www.w3.org/2000/svg" class="_34Yjv1"><path d="M16 23.207L6.11 13.161 16 3.093 12.955 0 0 13.161l12.955 13.161z" fill="#000000" class="_14jSvk"></path></svg></div>
+                    <div className="width1250 dropdown-content">
+                        <div className="flex space-btw ">
+                            <ul>
+                                <h4>Clothing</h4>
+                                <h4>Women Western & Maternity Wear</h4>
+                                <li>Topwear</li>
+                                <li>Dresses</li>
+                                <li>Jeans</li>
+                                <li>Shorts</li>
+                            </ul>
+                            <ul>
+                                <h4>Ethic Wear</h4>
+                                <li>Sarees</li>
+                                <li>Krutas & Kurtis</li>
+                                <li>Dress Material</li>
+                                <li>Lehenga Choli</li>
+                                <li>Blouse</li>
+                                <li>Gowns</li>
+                                <li>Kurta Sets & Salwar Suits</li>
+                                <li>Dupattas</li>
+                            </ul>
+                            <ul>
+                                <h4>Ethnic Bottoms</h4>
+                                <li>Leggings & Churidars</li>
+                                <li>Palazzos</li>
+                                <li>Shararas</li>
+                                <li>Salwars & Patiala</li>
+                            </ul>
+                            <ul>
+                                <h4>Footwear</h4>
+                                <h4>Sandals</h4>
+                                <li>Flats</li>
+                                <li>Heels</li>
+                                <li>Wedges</li>  
+                                <h4>Shoes</h4>
+                                <li>Sports Shoes</li>
+                                <li>Boots</li>
+                                <li>Casual Shoes</li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li className="dropdown"><div className="dropbtn" role="button">Baby & Kids <svg width="4.7" height="8" viewBox="0 0 16 27" xmlns="http://www.w3.org/2000/svg" class="_34Yjv1"><path d="M16 23.207L6.11 13.161 16 3.093 12.955 0 0 13.161l12.955 13.161z" fill="#000000" class="_14jSvk"></path></svg></div>
+                    <div className="width1250 dropdown-content">
+                        <div className="flex space-btw ">
+                            <ul>
+                                <h4>Kids Clothing</h4>
+                                <h4>Boys Clothing</h4>
+                                <li>T-Shirts</li>
+                                <li>Shirts</li>
+                                <li>Shorts</li>
+                                <li>Innerwear</li>
+                                <h4>Girls Clothing</h4>
+                                <li>Dresses & Skirts</li>
+                                <li>Ethnic Wear</li>
+                                <li>T-shirts & Tops</li>
+                                <li>Innerwear</li>
+                            </ul>
+                            <ul>
+                                <h4>Toys</h4>
+                                <li>Soft Toys</li>
+                                <li>Puzzles</li>
+                                <li>Dolls</li>
+                                <li>Toy Guns</li>
+                                <li>Board Games</li>
+                            </ul>
+                            <ul>
+                                <h4>Baby Care</h4>
+                                <li>Diapers</li>
+                                <li>Wipes</li>
+                                <li>Baby Gear</li>
+                                <li>Baby Grooming</li>
+                                <li>Baby Food</li>
+                            </ul>
+                            <ul>
+                                <h4>Kids Footwear</h4>
+                                <h4>Boys Footwear</h4>
+                                <li>Sandals</li>
+                                <li>Sport Shoes</li>
+                                <h4>Girls Footwear</h4>
+                                <li>Flats & Bellies</li>
+                                <li>Sport Shoes</li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li className="dropdown"><div className="dropbtn" role="button">Home & Furniture <svg width="4.7" height="8" viewBox="0 0 16 27" xmlns="http://www.w3.org/2000/svg" class="_34Yjv1"><path d="M16 23.207L6.11 13.161 16 3.093 12.955 0 0 13.161l12.955 13.161z" fill="#000000" class="_14jSvk"></path></svg></div>
+                    <div className="width1250 dropdown-content">
+                        <div className="flex space-btw ">
+                            <ul>
+                                <h4>Kitchen, Cookware & Serveware</h4>
+                                <li>Pans</li>
+                                <li>Tawas</li>
+                                <li>Pressure Cookers</li>
+                                <li>Kitchen Tools</li>
+                            </ul>
+                            <ul>
+                                <h4>Tableware & Dinnerware</h4>
+                                <li>Coffee Mugs</li>
+                                <li>Dinner Set</li>
+                                <li>Barware</li>
+                            </ul>
+                            <ul>
+                                <h4>Kitchen Storage</h4>
+                                <li>Water Bottles</li>
+                                <li>Lunch Boxes</li>
+                                <li>Flasks</li>
+                            </ul>
+                            <ul>
+                                <h4>Furnishing</h4>
+                                <li>Bedsheets</li>
+                                <li>Curtains</li>
+                                <li>Cushions & Pillows</li>
+                                <li>Blankets</li>
+                                <li>Bath Towels</li>
+                                <li>Floor Coverings</li>
+                            </ul>
+                            <ul>
+                                <h4>Home Lighting</h4>
+                                <li>Bulbs</li>
+                                <li>Wall Lamp</li>
+                                <li>Table Lamp</li>
+                                <li>Ceiling Lamp</li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li className="dropdown"><div className="dropbtn" role="button">Sports, Books & More <svg width="4.7" height="8" viewBox="0 0 16 27" xmlns="http://www.w3.org/2000/svg" class="_34Yjv1"><path d="M16 23.207L6.11 13.161 16 3.093 12.955 0 0 13.161l12.955 13.161z" fill="#000000" class="_14jSvk"></path></svg></div>
+                    <div className="width1250 dropdown-content">
+                        <div className="flex space-btw ">
+                            <ul>
+                                <h4>Sports</h4>
+                                <li>Cricket</li>
+                                <li>Badminton</li>
+                                <li>Cycling</li>
+                                <li>Football</li>
+                                <li>Skating</li>
+                                <li>Swimming</li>
+                            </ul>
+                            <ul>
+                                <h4>Stationery</h4>
+                                <li>Pens</li>
+                                <li>Pencils</li>
+                                <li>Diaries</li>
+                                <li>Card Holders</li>
+                                <li>Calculators</li>
+                                <li>Erasers & Sharpners</li>
+                            </ul>
+                            <ul>
+                                <h4>Books</h4>
+                                <li>Entrance Exams</li>
+                                <li>Academics</li>
+                                <li>Self-Help</li>
+                                <li>E-Learning</li>
+                                <li>Indian Languages</li>
+                                <li>Non Fiction</li>
+                                <li>Young Readers</li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li className="dropdown"><div className="dropbtn" role="button">Flights</div></li>
+                <li className="dropdown"><div className="dropbtn" role="button">Offer Zone</div></li>
+            </ul>
+        </div>
+    )
+}
+
+export default SecondNav
